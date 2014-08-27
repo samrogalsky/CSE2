40e6b92646e54aa2314c30a73657dc749b899dea
samrogalsky <sfr218@lehigh.edu>
2014-08-27 21:41:15 +0000

    finished hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+//Samuel Rogalsky
+//hw 01
+//Welcome Class Java Program
+
+public class WelcomeClass{
+    public static void main(String[] args) {
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-S--F--R--2--1--8-|  ");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Hi. I am Sam Rogalsky and I like to longboard. I usually just cruise around but");
+        System.out.println("the farthest I have gone is about fifteen miles.");
+    }
+}
