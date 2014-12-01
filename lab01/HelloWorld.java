88ce35832308d80e0e7d023d04c85a966bdc0821
samrogalsky <sfr218@lehigh.edu>
2014-12-01 17:29:32 +0000

    Lab01 - Finished

---
+++ lab01/HelloWorld.java
@@ -0,0 +1,17 @@
+//Samuel Rogalsky
+//Lab 01
+//CSE 02
+//Hello World Java Program
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+}
